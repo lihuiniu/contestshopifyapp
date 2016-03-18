c4ea85246b9b57b24f5dd6fb00a40ac13cd4663a
Hui Li <hli@lendingclub.com>
2016-03-17 18:27:27 -0700

    Commit 317 for the automated files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Contestapp::Application.config.secret_key_base = 'ff98b3a81cfededaa630ae415ebc772d955a2c6ead55ec5466413c5ada155205446a27530c8a7ca14ac7ed4422069a57f46c88c74e2b3237606a57789d8b11f5'
